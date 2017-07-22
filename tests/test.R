c936a141c557d8b1bd18f140b96157a76c9b406c
Anthony Damico <ajdamico@gmail.com>
2017-07-22 07:35:34 -0400

    machine_specific_replacements

--- tests/test.R
+++ tests/test.R
@@ -1,13 +1,16 @@
+my_email_address <- Sys.getenv( "my_email_address" )
+
 machine_specific_replacements <- 
 	list( 
 		
 		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"ANES\"' , paste0( '"' , getwd() , '"' ) ) ,
+		c( 'path.expand( \"~\" ) , \"CHAPTER_TAG\"' , paste0( '"' , getwd() , '"' ) ) ,
 		
 		# change other things in the script to be run
-		c( "hello" , "howdy" )
+		c( "hello" , "howdy" ) ,
+		
+		c( '"email@address.com"' , 'my_email_address' )
 		
 	)
 
 source( lodown::syntaxtractor( "anes" , replacements = machine_specific_replacements , setup_test = "test" ) , echo = TRUE )
-
